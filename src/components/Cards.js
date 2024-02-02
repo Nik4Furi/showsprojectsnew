89cf34bc8e1782c50908c6498625afd089670d3a
Nikhil Gurjar <91304976+Nik4Furi@users.noreply.github.com>
2024-02-02 08:27:22 +0530

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,35 +5,13 @@ import { useState } from 'react';
 const Cards = (props) => {
   let shows = props.shows;
 
-  console.log("shows ", shows[0].show.image.medium)
-
   const [likedShows, setLikedShows] = useState([]);
-
-  function getShows() {
-    if (true) {
-      let allShows = [];
-      Object.values(shows).forEach(array => {
-        array.forEach(showsData => {
-          allShows.push(showsData);
-        })
-      })
-      return allShows;
-    }
-
-  }
 
   return (
     <div className="flex flex-wrap justify-center gap-4 mb-4">
       {
-        // getShows().map( (shows) => (
-        //     <Card key={shows.show.id} 
-        //     shows = {shows} 
-        //     likedShows={likedShows}
-        //     setLikedShows={setLikedShows}/>
-        // ))
-
         shows && shows.map(item => {
-          return( <Card key={item.score} shows={item.show} likedShows={likedShows}
+          return (<Card key={item.score} shows={item.show} likedShows={likedShows}
             setLikedShows={setLikedShows} />)
         })
       }
