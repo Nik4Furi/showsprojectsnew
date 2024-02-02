1e9da124dfa1f820534bd182b2c47e3dd62f11d8
Nikhil Gurjar <91304976+Nik4Furi@users.noreply.github.com>
2024-02-02 08:29:11 +0530

    Update Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,26 +1,25 @@
 import React from 'react'
-import { useParams } from 'react-router-dom';
 import { useSearchParams } from 'react-router-dom';
-import { useEffect ,useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const Movie = () => {
-const [data, setDetail] = useState({});
-const [query, setQuery] = useSearchParams()
-console.log(query)
-const { id } = useParams();
-useEffect(() => {
-    // Replace 'YOUR_API_ENDPOINT' with the actual API endpoint
-    fetch(`api.url/${id}`)
-      .then(response => setDetail(response.data))
-      .catch(error => console.error('Error fetching detail:', error));
-  }, [id]);
-console.log(id)
-  return (
-    <div>
-      Movie
-    
-    </div>
-  )
+const Movie = ({ shows }) => {
+    const [data, setDetail] = useState({});
+    const id = useSearchParams()[0].get('id')
+
+    useEffect(() => {
+        shows && shows.forEach(item => {
+            if (item.show.id === Number(id)) setDetail(item);
+        })
+    }, [id])
+
+    console.log('movie id', id, 'with data', data);
+
+    return (
+        <div>
+            Movie {id}
+
+        </div>
+    )
 }
 
 export default Movie
