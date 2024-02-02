851d79b298836fda18abd8e992b36b12d7099820
Nikhil Gurjar <91304976+Nik4Furi@users.noreply.github.com>
2024-02-02 08:28:53 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,53 +1,31 @@
-import React from 'react'
-import Navbar from  "./Navbar";
-import Cards from "./Cards";
-import { useState,useEffect } from "react";
+import React, { Suspense, lazy } from 'react'
+import Navbar from "./Navbar";
 import Spinner from "./Spinner";
-import {toast} from "react-toastify";
-import { apiUrl } from "../data";
 
+const Cards = lazy(() => import("./Cards"))
 
-const Home = () => {
-    const [shows, setShows] = useState(null);
-  const [loading, setLoading] = useState(true);
-  async function fetchData() {
-    setLoading(true);
-    try{
-      let response = await fetch(apiUrl);
-      let output = await response.json();
-      ///output -> 
-      setShows(output);
-      console.log(output)
-    }
-    catch(error) {
-        toast.error("Network Error");
-    }
-    setLoading(false);
-  }
+const Home = ({ shows }) => {
 
-  useEffect(() => {
-    fetchData();
-  }, [])
-  return (
-    <div>
-        <div className="min-h-screen flex flex-col bg-bgDark2">
-      <div>
-        <Navbar/>
-      </div>
-      <div className="bg-bgDark2">
-        
-        <div className="w-11/12 max-w-[1200px] 
+    return (
+        <div>
+            <div className="min-h-screen flex flex-col bg-bgDark2">
+                <div>
+                    <Navbar />
+                </div>
+                <div className="bg-bgDark2">
+
+                    <div className="w-11/12 max-w-[1200px] 
         mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
-        {
-            loading ? (<Spinner/>) : (<Cards shows={shows} />)
-          }
+                        <Suspense fallback={<Spinner />} >
+                            {shows && <Cards shows={shows} />}
+                        </Suspense>
+                    </div>
+                </div>
+
+
+            </div>
         </div>
-      </div>
-
-
-    </div>
-    </div>
-  )
+    )
 }
 
 export default Home
