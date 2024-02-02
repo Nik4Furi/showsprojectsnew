9b84067e8ca4294661b5608f103c979a96fdf992
Nikhil Gurjar <91304976+Nik4Furi@users.noreply.github.com>
2024-02-02 08:27:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
-import React from "react";
-import { BrowserRouter , Routes, Route} from "react-router-dom";
-import Home from "./components/Home";
-import Movie from "./components/Movie";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Spinner from "./components/Spinner";
+
+
+import { useState, useEffect } from "react";
+
+
+import { toast } from "react-toastify";
+import { apiUrl } from "./data";
+
+
+const Home = lazy(() => import("./components/Home"))
+const Movie = lazy(() => import("./components/Movie"))
 
 const App = () => {
-  
-  return (
-    <BrowserRouter>
-    <Routes>
-      <Route path = "/" element ={<Home/>} ></Route>
-      <Route path = "/movie" element = {<Movie/>}></Route>
-    </Routes>
 
-    </BrowserRouter>
-  
+
+  const [shows, setShows] = useState(null);
+
+  async function fetchData() {
+    try {
+      let response = await fetch(apiUrl);
+      let output = await response.json();
+      ///output -> 
+      setShows(output);
+    }
+    catch (error) {
+      toast.error("Network Error");
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  return (
+    <>
+      <BrowserRouter>
+        <Suspense fallback={<Spinner />} >
+          <Routes>
+
+
+            <Route path="/" element={<Home shows={shows} />} ></Route>
+            <Route path="/movie" element={<Movie shows={shows} />}></Route>
+
+          </Routes>
+        </Suspense>
+
+      </BrowserRouter>
+    </>
   );
 
 
